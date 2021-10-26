df50e829afed78cbbed4c5b2b6a7be55d439f508
Bruno Rosendo <brunomrosendo@hotmail.com>
2021-10-26 15:33:25 +0100

    Organized for lab1

---
+++ Lab1/emissor.c
@@ -0,0 +1,17 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <termios.h>
+#include <stdio.h>
+
+#define BAUDRATE B38400
+#define MODEMDEVICE "/dev/ttyS1"
+#define _POSIX_SOURCE 1 /* POSIX compliant source */
+#define FALSE 0
+#define TRUE 1
+
+volatile int STOP=FALSE;
+
+int main(int argc, char** argv) {
+
+}
