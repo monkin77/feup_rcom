0e7be427bd1992a94a8d150d9b7acfe6415b9755
monkin <monkinsmurf@gmail.com>
2021-10-26 15:51:31 +0100

    Implemented emissor

--- Lab1/emissor.c
+++ Lab1/emissor.c
@@ -13,5 +13,85 @@
 volatile int STOP=FALSE;
 
 int main(int argc, char** argv) {
+    int fd,c, res;
+    struct termios oldtio,newtio;
+    char buf[255];
+    int i, sum = 0, speed = 0;
+    
+    if ( (argc < 2) || 
+  	     ((strcmp("/dev/ttyS0", argv[1])!=0) && 
+  	      (strcmp("/dev/ttyS1", argv[1])!=0) )) {
+      printf("Usage:\tnserial SerialPort\n\tex: nserial /dev/ttyS1\n");
+      exit(1);
+    }
 
+  /*
+    Open serial port device for reading and writing and not as controlling tty
+    because we don't want to get killed if linenoise sends CTRL-C.
+  */
+
+
+    fd = open(argv[1], O_RDWR | O_NOCTTY );
+    if (fd <0) {perror(argv[1]); exit(-1); }
+
+    if ( tcgetattr(fd,&oldtio) == -1) { /* save current port settings */
+      perror("tcgetattr");
+      exit(-1);
+    }
+
+    bzero(&newtio, sizeof(newtio));
+    newtio.c_cflag = BAUDRATE | CS8 | CLOCAL | CREAD;
+    newtio.c_iflag = IGNPAR;
+    newtio.c_oflag = 0;
+
+    /* set input mode (non-canonical, no echo,...) */
+    newtio.c_lflag = 0;
+
+    newtio.c_cc[VTIME]    = 0;   /* inter-character timer unused */
+    newtio.c_cc[VMIN]     = 1;   /* blocking read until 5 chars received */
+
+
+
+  /* 
+    VTIME e VMIN devem ser alterados de forma a proteger com um temporizador a 
+    leitura do(s) pr�ximo(s) caracter(es)
+  */
+
+
+
+    tcflush(fd, TCIOFLUSH);
+
+    if ( tcsetattr(fd,TCSANOW,&newtio) == -1) {
+      perror("tcsetattr");
+      exit(-1);
+    }
+
+    printf("New termios structure set\n");
+    
+    // Read input string
+    gets(buf);
+    printf("String being sent: %s\n", buf);
+    
+    res = write(fd,buf, sizeof(buf));   
+    printf("%d bytes written\n", res);
+ 
+
+  /* 
+    O ciclo FOR e as instru��es seguintes devem ser alterados de modo a respeitar 
+    o indicado no gui�o 
+  */
+
+    sleep(1); // Avoid changing config before sending data (transmission error)
+
+   
+    if ( tcsetattr(fd,TCSANOW,&oldtio) == -1) {
+      perror("tcsetattr");
+      exit(-1);
+    }
+
+
+
+
+    close(fd);
+    return 0;
 }
